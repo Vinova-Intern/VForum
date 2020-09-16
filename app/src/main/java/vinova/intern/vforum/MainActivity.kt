b3be02adb8d5ba07db69b4fbe57f6852af59b1e7
XtremerX2801 <32240335+XtremerX2801@users.noreply.github.com>
2020-09-16 16:07:36 +0700

    commit

--- app/src/main/java/vinova/intern/vforum/MainActivity.kt
+++ app/src/main/java/vinova/intern/vforum/MainActivity.kt
@@ -1,11 +1,7 @@
 package vinova.intern.vforum
 
 import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-    }
+
 }
